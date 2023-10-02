02edfaf2000f321516e892ed144bef0b14d53837
BT_1N3_15 <sample@bths.edu>
2023-10-02 09:36:14 -0400

    working on printing stuff

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        double itemCost = 0;
 
+        System.out.print("Welcome to the tip calculator!\nHow many people are in your group? ");
+        int numPeople = scan.nextInt();
+        scan.nextLine();
+        System.out.print("How much are you tipping our \033[3mfine\033[0m waiters?\nPlease tell me the percent: ");
+        double tipPercentage = scan.nextDouble();
 
+        TipCalculator tc = new TipCalculator(numPeople, tipPercentage);
+
+        while (itemCost != -1) {
+            System.out.print("The cost of the item in dollars and cents(enter -1 to end): ");
+            itemCost = scan.nextDouble();
+            tc.addMeal(itemCost);
+        }
+
+        System.out.println("The bill before tips is $" + tc.getTotalBillBeforeTip());
+        System.out.println("The tip percentage is " + tc.getTipPercentage() + "%");
+        System.out.println("The amount of tip is " + tc.tipAmount());
     }
 }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -16,5 +16,28 @@ public class TipCalculator {
         return totalBillBeforeTip;
     }
 
+    public double getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public double tipAmount() {
+        return (tipPercentage / 100.0 ) * totalBillBeforeTip;
+    }
+
+    public double totalBill() {
+        return totalBillBeforeTip + tipAmount();
+    }
+
+    public double perPersonCostBeforeTip() {
+        return Math.round(totalBillBeforeTip * 100.0 / numPeople) / 100.0;
+    }
+
+    public double perPersonTipAmount() {
+        return Math.round(tipAmount() * 100.0 / numPeople) / 100.0;
+    }
+
+    public double perPersonTotalCost() {
+        return Math.round(totalBill() * 100.0 / numPeople) / 100.0;
+    }
 
 }
